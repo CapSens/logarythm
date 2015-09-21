9a66c1715112179cb7711dfa9b58059f4c709615
Yassine Zenati <yassine@capsens.eu>
2015-09-21 17:16:07 +0200

    Using redis

--- lib/logarythm.rb
+++ lib/logarythm.rb
@@ -13,16 +13,12 @@ module Logarythm
   class Configuration
     attr_accessor :application_uuid
     attr_accessor :application_envs
-    attr_accessor :application_socket_id
-    attr_accessor :application_socket_key
-    attr_accessor :application_socket_secret
+    attr_accessor :application_host
 
     def initialize
-      @application_uuid          = nil
-      @application_envs          = nil
-      @application_socket_id     = nil
-      @application_socket_key    = nil
-      @application_socket_secret = nil
+      @application_uuid = nil
+      @application_envs = nil
+      @application_host = nil
     end
   end
 
@@ -45,18 +41,14 @@ module Logarythm
         configuration_options = [
           :application_uuid,
           :application_envs,
-          :application_socket_id,
-          :application_socket_key,
-          :application_socket_secret
+          :application_host
         ].map { |option| configuration.send(option).present? }.exclude?(false)
 
         if configuration_options && configuration.application_envs.include?(Rails.env.to_sym)
-          Pusher.app_id = configuration.application_socket_id
-          Pusher.key    = configuration.application_socket_key
-          Pusher.secret = configuration.application_socket_secret
+          redis = Redis.new url: configuration.host
 
-          ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |name, start, finish, id, payload|
-            Pusher.trigger_async(configuration.application_uuid, 'process_action.action_controller', {
+          ActiveSupport::Notifications.subscribe /sql|controller|view/ do |name, start, finish, id, payload|
+            redis.publish configuration.application_uuid, {
               content: {
                 env: Rails.env,
                 name: name,
@@ -64,31 +56,7 @@ module Logarythm
                 finish: finish,
                 payload: (Base64.encode64(deep_simplify_record(payload).to_json) rescue nil)
               }
-            })
-          end
-
-          ActiveSupport::Notifications.subscribe /sql.active_record/ do |name, start, finish, id, payload|
-            Pusher.trigger_async(configuration.application_uuid, 'sql.active_record', {
-              content: {
-                env: Rails.env,
-                name: name,
-                start: start,
-                finish: finish,
-                payload: (Base64.encode64(deep_simplify_record(payload).to_json) rescue nil)
-              }
-            })
-          end
-
-          ActiveSupport::Notifications.subscribe /render_template.action_view/ do |name, start, finish, id, payload|
-            Pusher.trigger_async(configuration.application_uuid, 'render_template.action_view', {
-              content: {
-                env: Rails.env,
-                name: name,
-                start: start,
-                finish: finish,
-                payload: (Base64.encode64(deep_simplify_record(payload).to_json) rescue nil)
-              }
-            })
+            }.to_json
           end
         end
       end

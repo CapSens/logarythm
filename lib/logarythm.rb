d584e8b3fdeff9797748b39bf92245d9487e380b
Yassine Zenati <yassine@capsens.eu>
2015-09-22 12:46:03 +0200

    ...

--- lib/logarythm.rb
+++ lib/logarythm.rb
@@ -24,41 +24,48 @@ module Logarythm
 
   class Railtie < Rails::Railtie
     config.after_initialize do
-
-      def deep_simplify_record hsh
-        hsh.keep_if do |h, v|
-          if v.is_a?(Hash)
-            deep_simplify_record(v)
-          else
-            v.is_a?(String) || v.is_a?(Integer)
+      begin
+        def deep_simplify_record hsh
+          hsh.keep_if do |h, v|
+            if v.is_a?(Hash)
+              deep_simplify_record(v)
+            else
+              v.is_a?(String) || v.is_a?(Integer)
+            end
           end
         end
-      end
 
-      configuration = Logarythm.configuration
+        configuration = Logarythm.configuration
 
-      if configuration.present?
-        configuration_options = [
-          :application_uuid,
-          :application_envs,
-          :application_host
-        ].map { |option| configuration.send(option).present? }.exclude?(false)
+        if configuration.present?
+          configuration_options = [
+            :application_uuid,
+            :application_envs,
+            :application_host
+          ].map { |option| configuration.send(option).present? }.exclude?(false)
 
-        if configuration_options && configuration.application_envs.include?(Rails.env.to_sym)
-          redis = Redis.new url: ['redis://', configuration.application_host].join
+          if configuration_options && configuration.application_envs.include?(Rails.env.to_sym)
+            redis = Redis.new url: ['redis://', configuration.application_host].join
 
-          ActiveSupport::Notifications.subscribe /sql|controller|view/ do |name, start, finish, id, payload|
-            redis.publish configuration.application_uuid, {
-              content: {
-                env: Rails.env,
-                name: name,
-                start: start,
-                finish: finish,
-                payload: (Base64.encode64(deep_simplify_record(payload).to_json) rescue nil)
-              }
-            }.to_json
+            ActiveSupport::Notifications.subscribe /sql|controller|view/ do |name, start, finish, id, payload|
+              redis.publish configuration.application_uuid, {
+                content: {
+                  env: Rails.env,
+                  name: name,
+                  start: start,
+                  finish: finish,
+                  payload: (Base64.encode64(deep_simplify_record(payload).to_json) rescue nil)
+                }
+              }.to_json
+            end
           end
         end
+      rescue Exception => e
+        puts
+        puts
+        puts e.inspect
+        puts
+        puts
       end
     end
   end

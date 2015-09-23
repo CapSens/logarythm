bba5fec0c1cf7012360453faadc7fb27d8e88618
Yassine Zenati <yassine@capsens.eu>
2015-09-23 14:46:52 +0200

    redis current

--- app/jobs/log_job.rb
+++ app/jobs/log_job.rb
@@ -4,7 +4,7 @@ class LogJob
   include SuckerPunch::Job
 
   def perform hash, configuration
-    # @redis ||= Redis.new url: ['redis://', configuration.application_host].join
-    # @redis.publish configuration.application_uuid, hash
+    Redis.current ||= Redis.new url: ['redis://', configuration.application_host].join
+    Redis.current.publish configuration.application_uuid, hash
   end
 end

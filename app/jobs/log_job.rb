bf22bfb249f2806982c224821fed8fccdcfa8945
Yassine Zenati <yassine@capsens.eu>
2015-09-29 15:21:38 +0200

    ...

--- app/jobs/log_job.rb
+++ app/jobs/log_job.rb
@@ -1,7 +1,10 @@
 class LogJob
   include SuckerPunch::Job
+  workers 1
 
   def perform socket, hash
-    socket.emit :data, hash
+    ActiveRecord::Base.connection_pool.with_connection do
+      socket.emit :data, hash
+    end
   end
 end

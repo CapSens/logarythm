9948be4d387045ab60f25145cecf0d6b5aad14d8
Yassine Zenati <yassine@capsens.eu>
2015-10-04 19:34:41 +0200

    session

--- lib/logarythm/engine.rb
+++ lib/logarythm/engine.rb
@@ -2,4 +2,11 @@ module Logarythm
   class Engine < ::Rails::Engine
     isolate_namespace Logarythm
   end
+
+  module ApplicationController
+    def append_info_to_payload payload
+        super
+        payload[:session] = request.session.id rescue nil
+    end
+  end
 end
